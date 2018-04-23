2fb20b7b95ef2f4934462ac663aafc3694387181
cl2112 <cl2112cl2112@gmail.com>
2018-04-23 14:06:25 -0400

    Continued work on the game logic.

--- hangman/app.js
+++ hangman/app.js
@@ -3,6 +3,7 @@ const HangmanGame = {
     wordList: ['one', 'two','three','four'],
     wordsGuessed: [],
     currentWord: '',
+    blankWord: '',
     guessesLeft: 10,
     lettersGuessed: [],
     wins: 0,
@@ -11,17 +12,45 @@ const HangmanGame = {
     Start: function () {
         this.started = true;
         this.ChooseWord();
+        this.CreateBlanks();
+        this.RenderGameState();
+        console.log(this);
     },
 
-    KeyPressed: function (letter) {
-
-    },
 
     ChooseWord: function () {
-        
+        this.currentWord = this.wordList[Math.floor(Math.random()*this.wordList.length)];
+        console.log(this.currentWord);
     },
 
+    CheckGuess: function (letter) {
+        if (this.currentWord.indexOf(letter) > -1) {
+            this.FillInBlanks(letter);
+        } else {
+            this.guessesLeft --;
+        }
+        this.RenderGameState();
+    },
 
+    IsWordComplete: function () {
+        return this.blankWord.indexOf('_') > -1;
+    },
+
+    RenderGameState: function () {
+        document.querySelector('h1').innerText = this.blankWord.join(' ');
+        document.querySelector('#guessesLeft').innerText = this.guessesLeft;
+        document.querySelector('#wins').innerText = this.wins;
+        document.querySelector('#losses').innerText = this.losses;
+        document.querySelector('#lettersGuessed').innerText = this.lettersGuessed;
+    },
+
+    CreateBlanks: function () {
+        this.blankWord = this.currentWord.split('').map(v=>'_');
+    },
+
+    FillInBlanks: function (letter) {
+        this.blankWord = this.blankWord.map((v,i)=>this.currentWord[i] === letter ? letter : v);
+    }
 
 };
 
@@ -29,5 +58,5 @@ document.onkeypress = event => {
     const userGuess = event.key;
     console.log(userGuess);
     if (!HangmanGame.started) {HangmanGame.Start()}
-    else {HangmanGame.KeyPressed(userGuess)}
+    else {HangmanGame.CheckGuess(userGuess)}
 };
